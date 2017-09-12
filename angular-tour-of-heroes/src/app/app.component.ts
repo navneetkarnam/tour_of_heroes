d5ba0d5196d9f40eceaa306d1361aa913ae0949a
Navneet Karnam <navneetkarnam@gmail.com>
2017-09-12 02:02:22 -0400

    refactor: adding styles and update feature

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -4,6 +4,19 @@ export class Hero {
   id: number;
   name: string;
 }
+const HEROES: Hero[] = [
+  {id: 11, name: 'Mr Nice'},
+  {id: 12, name: 'Mr Narco'},
+  {id: 13, name: 'Mr Bombasto'},
+  {id: 14, name: 'Celeritas'},
+  {id: 15, name: 'Magnetas'},
+  {id: 16, name: 'Deleritas'},
+  {id: 17, name: 'Batman'},
+  {id: 18, name: 'Superman'},
+  {id: 19, name: 'Spiderman'},
+  {id: 20, name: 'Tornado'},
+];
+
 
 @Component({
   selector: 'app-root',
@@ -12,9 +25,12 @@ export class Hero {
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
+  heroes = HEROES;
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
+
